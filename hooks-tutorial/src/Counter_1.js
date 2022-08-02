de5c4154cc9ef6bd6b588903bf659c8ff1f9ec4f
cadiablue1 <cadiablue1@naver.com>
2022-08-02 09:46:01 +0900

    22.08.02

---
+++ hooks-tutorial/src/Counter_1.js
@@ -0,0 +1,18 @@
+import React, {useState} from 'react';
+
+
+//class Counter extend React
+const Counter = () => {
+    const [value,setValue] = useState(0); // userState의 초기값 =0 
+    return(
+        <div>
+            <p>
+                현재 카운터 값은 <b>{value}</b>입니다.
+            </p>
+            <button onClick={()=> setValue(value+1)}>+1</button>
+            <button onClick={()=> setValue(value-1)}>-1</button>
+        </div>
+    );
+};
+
+export default Counter;
