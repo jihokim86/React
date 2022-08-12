7d1b285bc7e95cd9414dea4d69349fa87e583f04
cadiablue1 <cadiablue1@naver.com>
2022-08-12 13:10:31 +0900

    22.08.12

---
+++ blog/blog-backend/src/api/index.js
@@ -0,0 +1,16 @@
+import Router from "koa-router";
+import posts from './posts';
+
+
+//const Router = require('koa-router');
+//const posts = require('./posts');
+
+
+const api = new Router();
+
+api.use('/posts',posts.routes());
+
+
+// 라우터를 내보냅니다.
+//module.exports = api;
+export default api;
---
+++ redux-test/src/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { Provider } from 'react-redux';
+import { legacy_createStore as createStore } from 'redux';
+
+const 체중 = 100;
+
+//state 수정방법 
+function reducer(state = 체중, action){
+   if (action.type === '증가'){
+      state++;
+      return state
+   }else if(action.type === '감소'){
+      state--;
+      return state
+   }else{
+    return state
+   }
+}
+
+let store = createStore(reducer)
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
